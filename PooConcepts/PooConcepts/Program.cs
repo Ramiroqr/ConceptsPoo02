3feeea2d5c91b0fc573129dca81462f945b5fbb5
Ramiroqr <ramiroquisper@gmail.com>
2022-03-06 20:00:29 -0500

    Finish date class

---
+++ PooConcepts/PooConcepts/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PooConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POO concepts");
+            Console.WriteLine("============");
+
+            try
+            {
+                Console.WriteLine(new Date(2024, 2, 29));
+                Console.WriteLine(new Date(1974, 9, 23));
+                Console.WriteLine(new Date(1985, 11, 31));
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+               
+            }
+
+        }
+    }
+}
